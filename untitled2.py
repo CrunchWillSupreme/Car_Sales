5d6aae5b58d6981683b6b5211888bc9d51cda29e
Will Han <will.han38@gmail.com>
2019-11-26 02:15:31 -0500

    changes made

--- untitled2.py
+++ untitled2.py
@@ -49,20 +49,7 @@ data = raw_data.copy()
 
 import datetime as dt
 data['DMSTDESL_DT'] = pd.to_datetime(data['DMSTDESL'], format = '%Y%m%d')
-data['Sale_day_name'] = data['DMSTDESL_DT'].dt.weekday 
-
-# check out some columns
-sale_days = data['Sale_day_name'].unique() # sales happen on wednesdays
-sold = data[data['DMSOLD'] == 1]
-sold['DMSTDESL_DT'].groupby(sold['DMSTDESL_DT']).count().plot.bar()
-sold['DMSTDESL_DT'].value_counts().plot.bar()
-
-pd.crosstab(data['DMMAKE'],data['DMSOLD']).plot.bar()
-pd.crosstab(data['DMMODEL'],data['DMSOLD']).plot.bar()
-pd.crosstab(data['DMJDCAT'], data['DMSOLD']).plot.bar()
-pd.pivot_table(data, index=['DMMAKE', 'DMMODEL'], columns=['DMSOLD'], aggfunc=len)
-data['DMSOLD'][data['DMSOLD']==1].groupby([data['DMMAKE'],data['DMMODEL']]).count()
-
+data['Sale_day_name'] = data['DMSTDESL_DT'].dt.weekday
 # make a column for week of month (1st week, 2nd week, 3rd week, ...)
 days = pd.DatetimeIndex(data['DMSTDESL_DT'].unique())
 week_month = {}
@@ -81,25 +68,68 @@ for d in days:
         week_month[d] = week
         week +=1
         
-data['month_week'] = data['DMSTDESL_DT'].map(week_month)
+data['month_week'] = data['DMSTDESL_DT'].map(week_month) 
+
+# check out some columns
+sale_days = data['Sale_day_name'].unique() # sales happen on wednesdays
+sold = data[data['DMSOLD'] == 1]
+sold['DMSTDESL_DT'].groupby(sold['DMSTDESL_DT']).count().plot.bar()
+sold['DMSTDESL_DT'].value_counts().plot.bar()
+
+pd.crosstab(data['DMMAKE'],data['DMSOLD']).plot.bar()
+pd.crosstab(data['DMMODEL'],data['DMSOLD']).plot.bar()
+pd.crosstab(data['DMJDCAT'], data['DMSOLD']).plot.bar()
+pd.pivot_table(data, index=['DMMAKE', 'DMMODEL'], columns=['DMSOLD'], aggfunc=len)
+data['DMSOLD'][data['DMSOLD']==1].groupby([data['DMMAKE'],data['DMMODEL']]).count()
 
 c = data.columns
-# Make categorical variables
-cat_cols = ['DMMONTH', 'SLANE_', 'DMOPCSUID', 'DMMAKE','STIMES_bin', 'Arbitration_bins','DMMODELYR','DMMODELYR_bins',]
-for col in cat_cols:
-    data[col] = data[col].astype('category')
+## Make categorical variables
+#cat_cols = ['DMMONTH', 'SLANE_', 'DMOPCSUID', 'DMMAKE','STIMES_bin', 'Arbitration_bins','DMMODELYR_bins',]
+#for col in cat_cols:
+#    data[col] = data[col].astype('category')
 
 # Drop columns that I won't be using
-drop_cols = ['SFLOOR', 'DMECRDATE','DMSTDESL', 'DMSALWK', 'SSALE_', 'SRUN_', 'STIMES', 'DMTRANTYPE', 'DMSELLRNM', 'DMJDCAT', 'DMMODEL', 'DMBODY', 'SSER17', 'AGEDDAYS', 'DMPRECOND', 'DMPOSTCOND', 'DMDETFEE', 'DMRECONFEE','SFLR_lower','% Arbitration','imp_STIMES','VNMMR','SSLEPR'
+drop_cols = ['SFLOOR', 'DMECRDATE','DMSTDESL', 'DMSALWK', 'SSALE_', 'SRUN_', 'STIMES', 'DMTRANTYPE', 'DMSELLRNM', 'DMJDCAT', 
+             'DMMODEL', 'DMBODY', 'SSER17', 'AGEDDAYS', 'DMPRECOND', 'DMPOSTCOND', 'DMDETFEE', 'DMRECONFEE','SFLR_lower','% Arbitration','imp_STIMES','VNMMR','SSLEPR'
              , 'DMOPCSUID','DMMODELYR','SMILES','DMMONTH','SLANE_','STIMES_bin3','STIMES_bin', 'DMSTDESL_DT','Sale_day_name']
 data_penult = data.drop(drop_cols, axis =1)
 data[data.columns != drop_cols]
 
+# feature engineering
+data_penult = data[['SFLOOR_IND','DMECRDATE_IND', 'STIMES_bin2', 'Arbitration_bins', 'DMMODELYR_bins','DMSOLD']]
+results1 = initial_all_results
 
+data_penult = data[['SFLOOR_IND', 'STIMES_bin2', 'Arbitration_bins', 'DMMODELYR_bins','DMSOLD']]
+results2 = initial_all_results
+
+data['STIMESxArb'] = data['STIMES_bin2'] + " " + data['Arbitration_bins']
+ECRdata = data[data['SFLOOR_IND'] == 'Y']
+data_penult = ECRdata[['STIMESxArb', 'DMSOLD', 'DMECRDATE_IND', 'DMMODELYR_bins']]
+data_penult = ECRdata[['DMECRDATE_IND', 'STIMES_bin2', 'Arbitration_bins', 'DMMODELYR_bins','DMSOLD']]
+results3 = initial_all_results
+
+data_penult = data[['STIMESxArb', 'DMSOLD', 'DMECRDATE_IND', 'DMMODELYR_bins']]
+results4 = #could not complete - SVM
+
+data['MODELYRxDMMODEL'] = data['DMMODELYR_bins'] + ' ' + data['DMMODEL']
+data_penult = data[['MODELYRxDMMODEL', 'STIMESxArb','DMSOLD','DMECRDATE_IND']]
+
+data['SFLOORxSTIMES'] = data['STIMES_bin2'] + ' ' + data['SFLOOR_IND']
+data['SFLOORxDMECR'] = data['DMECRDATE_IND'] + ' ' + data['SFLOOR_IND']
+data['SFLOORxARB'] = data['Arbitration_bins'] + ' ' + data['SFLOOR_IND']
+data['SFLOORxDMMODEL'] = data['DMMODELYR_bins'] + ' ' + data['SFLOOR_IND']
+data_penult = data[['SFLOORxSTIMES','SFLOORxDMECR', 'SFLOORxARB', 'SFLOORxDMMODEL','DMSOLD']]
+results5 = initial_all_results
+
+data['STIMESxSFLOOR'] = data['STIMES_bin2'] + ' ' + data['SFLOOR_IND']
+data['STIMESxDMECR'] = data['STIMES_bin2'] + ' ' + data['DMECRDATE_IND']
+data['STIMESxARB'] = data['STIMES_bin2'] + ' ' + data['Arbitration_bins']
+data['STIMESxDMMODEL'] = data['STIMES_bin2'] + ' ' + data['DMMODELYR_bins']
+data_penult = data[['STIMESxSFLOOR', 'STIMESxDMECR','STIMESxARB','STIMESxDMMODEL','DMSOLD']]
 
 # Make dummy variables
 cat_cols = data_penult.loc[:, data_penult.columns != 'DMSOLD'].columns
-#cat_cols = ['DMECRDATE_IND','SFLOOR_IND','STIMES_bin', 'Arbitration_bins','DMMODELYR_bins']
+cat_cols = ['STIMESxArb', 'month_week']
 for col in cat_cols:
     cat_list='var'+'_'+col
     cat_list = pd.get_dummies(data_penult[col], prefix=col, drop_first = True)
@@ -111,20 +141,17 @@ to_keep=[i for i in data_vars if i not in cat_cols]
 data_final=data_penult[to_keep]
 data_final.columns.values
 
-#data_final.isnull().sum()
-
 # Make X and y datasets
 X = data_final.drop(['DMSOLD'], axis = 1)
 y = data_final.DMSOLD.values  
 X = X.values
 
-"""
 # scale the data
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 scaled_X = sc_X.fit_transform(X)
 #scaled_X = pd.DataFrame(data = scaled_X, columns = X.columns.tolist())
-"""
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 random_state = 0
@@ -148,10 +175,16 @@ def get_model_results(X_train, y_train, cat_cols):
         print("Running initial "+name)
         kfold = StratifiedKFold(n_splits=10, random_state=1)
         cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring='accuracy')
+        
         results.append(cv_results)
         names.append(name)
         initial_all_results[name] = [cv_results.mean(),cv_results.std()]
         print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+        classifier = model
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+        cm = confusion_matrix(y_test, y_pred)
+        print(cm)
         
     
     
